91ccf690a4b26cca47a81d85af30c23c718ef3ef
Byron <byronbuckley@live.com>
2014-02-23 13:27:49 -0500

    fixed foundation.js not loading
    Only loading the foundation components I used. Minified in
    foundation.custom.min.js

--- portfolio/basecamp/js/general.js
+++ portfolio/basecamp/js/general.js
@@ -1,6 +1,4 @@
-function fixTopBar() {
-    console.log('fix top ran');
-    
+function fixTopBar() {    
     var mainContentWidth = $('#main-content').width(); //get width of main content body
     var mainLeftOffset = $('#main-content').offset().left + 15; //get left offset of main content body
     $('#wrap-main-nav').css('width',mainContentWidth + 'px').css('left', mainLeftOffset + 'px'); //give the wrapping container of the topbar the new figures
