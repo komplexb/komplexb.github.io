9b2b21a2b0087fc45a4c2b895ba6fd7c661643d3
Byron Buckley <bbuckley@medullan.com>
2014-08-11 15:54:57 -0500

    add smooth scrolling

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,3 +3,24 @@ $( document ).ready(function() {
   //
 
 });
+
+$(function() {
+	smoothScrool(300);
+	workBelt();
+	workLoad();
+});
+
+
+function smoothScrool (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+	    var target = $( $(this).attr('href') );
+
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	            scrollTop: target.offset().top
+	        }, duration);
+	    }
+	});
+}
