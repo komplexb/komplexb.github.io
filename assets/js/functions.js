01207c9dc563102a241c36a9943eb88cb8fc30df
Byron <byronbuckley@live.com>
2014-09-01 08:26:04 -0500

    part 12

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -7,7 +7,7 @@ $( document ).ready(function() {
 $(function() {
 	smoothScroll(300);
 	workBelt();
-//	workLoad();
+	workLoad();
 });
 
 
@@ -34,5 +34,19 @@ function workBelt() {
     $('.work-return').click(function() {
         $('.work-belt').css('left','0%');
         $('.work-container').hide(800);
+    });
+}
+
+function workLoad() {
+    $.ajaxSetup({cache: true});
+    
+    $('.thumb-unit').click(function() {
+        var $this = $(this),
+            newTitle = $this.find('strong').text(),
+            newFolder = $this.data('folder'),
+            spinner = "<div class='loader'>Loading...</div>",
+            newHTML = '/work/' + newFolder;
+        $('.project-load').html(spinner).load(newHTML);
+        $('.project-title').text(newTitle);
     });
 }
