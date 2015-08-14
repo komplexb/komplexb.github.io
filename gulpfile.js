d2350a5dcad0fecaa4244b931ef854fa2e40ced1
Byron Buckley <byronbuckley@live.com>
2015-08-14 13:28:14 -0500

    svg sprite config & styling

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,12 @@
+var paths;
+
+paths = {
+  sprites: {
+    source: "svg",
+    build: "_includes/svg"
+  }
+};
+
 var gulp        = require('gulp');
 var browserSync = require('browser-sync');
 var sass        = require('gulp-sass');
@@ -5,6 +14,19 @@ var prefix      = require('gulp-autoprefixer');
 var cp          = require('child_process');
 var ghpages 	= require('gh-pages');
 var path 		= require('path');
+var svgSprite 	= require('gulp-svg-sprite');
+var Notification = require('node-notifier');
+var plugins 	= require('gulp-load-plugins')();
+
+var errorHandler = function(e) {
+  var notifier;
+  notifier = new Notification();
+  notifier.notify({
+    title: "Gulp Error",
+    message: e.message
+  });
+  return this.emit("end");
+};
 
 var messages = {
     jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
@@ -57,6 +79,28 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('css'));
 });
 
+
+/**
+ * SVG Sprites
+ */
+gulp.task("sprites", function () {
+    var config = {
+		mode: {
+			symbol: {
+				dest: "",
+				sprite: "sprite.html",
+				inline: true,
+				example: false
+			}
+		}
+	}
+	return gulp.src(paths.sprites.source + "/**/*.svg")
+	.pipe(svgSprite(config))
+	.on('error', errorHandler)
+	.pipe(plugins.extReplace(".html")).pipe(gulp.dest(paths.sprites.build));
+});
+
+
 /**
  * Watch scss files for changes & recompile
  * Watch html/md files, run jekyll & reload BrowserSync
@@ -64,6 +108,7 @@ gulp.task('sass', function () {
 gulp.task('watch', function () {
 //    gulp.watch('_scss/*.scss', ['sass']);
 //    gulp.watch(['index.html', '_includes/*.html', '_layouts/*.html', '_posts/*', 'assets/css/**/*.{scss,sass}'], ['jekyll-rebuild']);
+	gulp.watch(paths.sprites.source + "/**/*.svg", ['sprites']);
     gulp.watch(['index.html', 'work/**/*.md', '_includes/*.html', '_layouts/*.html', '_posts/*', 'assets/css/**/*.{scss,sass}'], ['jekyll-rebuild']);
 });
 
@@ -71,7 +116,13 @@ gulp.task('watch', function () {
  * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
-gulp.task('default', ['browser-sync', 'watch']);
+//gulp.task('default', ['browser-sync', 'watch']);
+
+gulp.task('default', function() {
+  gulp.start(['sprites', 'browser-sync']);
+  gulp.watch(['index.html', 'work/**/*.md', '_includes/*.html', '_layouts/*.html', '_posts/*', 'assets/css/**/*.{scss,sass}'], ['jekyll-rebuild']);
+  return gulp.watch(paths.sprites.source + "/**/*.svg", ['sprites']);
+});
 
 
 gulp.task("deploy", ["jekyll-build"], function () {
